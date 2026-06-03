410b03272d753faf14f4382f345f3d28939c0c71
hieuvan153 <hieuvan153282278@gmail.com>
2026-06-04 00:22:57 +0700

    feat: Created Farm Controller (needs FarmMap from soil package of model)

---
+++ sourcecode/controller/farmcontroller/FarmController.java
@@ -0,0 +1,23 @@
+package controller.farmcontroller;
+
+import controller.lifecyclemanager.BaseController;
+import controller.lifecyclemanager.GameManager;
+import model.soil.FarmMap;
+
+public class FarmController extends BaseController {
+    private final FarmMap farmMap;
+
+    public FarmController(GameManager gameManager, FarmMap farmMap) {
+        super(gameManager, farmMap.getGrid());
+        this.farmMap = farmMap;
+    }
+
+    public void processGrowth() {
+        farmMap.updateAllCells(gameManager.getCurrentDay(), gameManager.getCurrentWeather());
+    }
+
+    @Override
+    public void dayEnded() {
+        processGrowth();
+    }
+}
