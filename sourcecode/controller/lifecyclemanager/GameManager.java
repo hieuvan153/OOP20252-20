64f731653a8d28da6737d8d4cbac3fdc4116ec50
hieuvan153 <hieuvan153282278@gmail.com>
2026-06-03 23:48:17 +0700

    feat: Created Game Manager (needs Weather package of model)

---
+++ sourcecode/controller/lifecyclemanager/GameManager.java
@@ -0,0 +1,57 @@
+package controller.lifecyclemanager;
+
+import model.weather.Weather;
+import model.weather.WeatherType;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class GameManager {
+    private static final class Holder {
+        static final GameManager INSTANCE = new GameManager();
+    }
+    public static GameManager getInstance() {
+        return Holder.INSTANCE;
+    }
+    private GameManager() {
+        reset();
+    }
+
+    private int currentDay;
+    private Weather currentWeather;
+    private final List<DayObserver> observers = new ArrayList<>();
+
+    public void addObserver(DayObserver observer) {
+        Objects.requireNonNull(observer, "Observer must not be null");
+        if (!observers.contains(observer)) {
+            observers.add(observer);
+        }
+    }
+
+    public int getCurrentDay() {
+        return currentDay;
+    }
+    public Weather getCurrentWeather() {
+        return currentWeather;
+    }
+    public void setCurrentWeather(Weather currentWeather) {
+        this.currentWeather = currentWeather;
+    }
+
+    public void reset() {
+        observers.clear();
+        currentDay = 1;
+        currentWeather = Weather.of(WeatherType.SUNNY);
+    }
+
+    public void advanceDay() {
+        for (DayObserver obs : observers) {
+            obs.dayEnded();
+        }
+        currentDay++;
+        if (currentWeather != null) {
+            currentWeather.tickDuration();
+        }
+    }
+}
