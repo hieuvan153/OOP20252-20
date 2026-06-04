59de850a55b66d2f15747e43f70309d2837af9f5
hieuvan153 <hieuvan153282278@gmail.com>
2026-06-04 09:35:27 +0700

    feat: Created Player Controller (needs Player from player_inventory package)

---
+++ sourcecode/controller/actionmanager/PlayerController.java
@@ -0,0 +1,42 @@
+package controller.actionmanager;
+
+import controller.lifecyclemanager.BaseController;
+import controller.lifecyclemanager.GameManager;
+import model.player_inventory.PLayer;
+import model.soil.Cell;
+
+public class PlayerController extends BaseController {
+    private final Player player;
+    private ToolStrategy currentTool;
+
+    public PlayerController(Player player, GameManager gameManager, Cell[][] grid) {
+        super(gameManager, grid);
+        this.player = player;
+        this.currentTool = new Hand(player);
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+    public ToolStrategy getCurrentTool() {
+        return currentTool;
+    }
+
+    public void useTool(ToolStrategy tool) {
+        if (tool != null) {
+            this.currentTool = tool;
+        }
+    }
+
+    public boolean interactWithCell(Cell cell) {
+        if (currentTool == null || cell == null) {
+            return false;
+        }
+        return currentTool.executeOnTarget(cell);
+    }
+
+    @Override
+    public void dayEnded() {
+
+    }
+}
