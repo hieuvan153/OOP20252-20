432e210f42aa1d751c052bbf32291f1fa687a4ae
Phung Duc Thinh 202417200 <phungthinh035@gmail.com>
2026-06-04 21:02:48 +0700

    modify some logics in updating daily stress

--- sourcecode/model/crop/FloodLovingStressManager.java
+++ sourcecode/model/crop/FloodLovingStressManager.java
@@ -14,10 +14,9 @@ public class FloodLovingStressManager implements StressManager {
         int stress = 0;
         if (cell.getMoistureLevel() < 40)  stress += 15;
         if (cell.getNutrientLevel() < 20)  stress += 10;
-        if(cell.getSunlightLevel() > 70)   stress += 5;
         if (weather != null && weather.getType() == WeatherType.DROUGHT) stress += 20;
         if (weather != null && weather.getType() == WeatherType.RAINY)   stress -= 15;  // loves rain
-        if(cell.getMoistureLevel() > 70) stress -= 10;
+        if(cell.getMoistureLevel() > 70) stress -= 15;
         return stress;
     }
 }
