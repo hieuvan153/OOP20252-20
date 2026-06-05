061002e04f405eb4aab8273ba88b1ee5e7010f55
fixthat27 <fixthat4@gmail.com>
2026-06-05 23:26:53 +0700

    feat: add pause and seed_picker key and path

--- sourcecode/view/screen_util/ScreensLoader.java
+++ sourcecode/view/screen_util/ScreensLoader.java
@@ -3,10 +3,7 @@ package view.screen_util;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 
-import view.screen_controller.HelpController;
-import view.screen_controller.InGameController;
-import view.screen_controller.MainMenuController;
-import view.screen_controller.ShopController;
+import view.screen_controller.*;
 
 public final class ScreensLoader {
     private ScreensLoader() {}
@@ -38,6 +35,14 @@ public final class ScreensLoader {
 
         ShopController shopController = shopLoader.getController();
         shopController.setRouter(router);
+
+        // ================= PAUSE ===================
+        FXMLLoader pauseLoader = load(router, Screens.PAUSE, Screens.PAUSE_FXML);
+
+        PauseController pauseController = pauseLoader.getController();
+        pauseController.setRouter(router);
+
+        // TODO: load seed bag (seed picker)
 
         return router;
     }
