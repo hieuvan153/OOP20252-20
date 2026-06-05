21596e3c2f233b259349df0f7989080f4d882c67
Phung Duc Thinh 202417200 <phungthinh035@gmail.com>
2026-06-06 00:37:43 +0700

    fix: modify some format in CropData

--- sourcecode/model/crop/CropData.java
+++ sourcecode/model/crop/CropData.java
@@ -13,15 +13,23 @@ public class CropData {
         // maxStress is a divisor in the growth formula and the death threshold;
         // 0 (or negative) would mean "dies instantly"
         if (maxStress <= 0) {
-            throw new IllegalArgumentException(
-                    "maxStress must be > 0 (got " + maxStress + ")");
+            throw new IllegalArgumentException("maxStress must be > 0 (got " + maxStress + ")");
         }
+
         this.cost = cost;
         this.value = value;
         this.maxStress = maxStress;
     }
 
-    public int getCost()      { return cost; }
-    public int getValue()     { return value; }
-    public int getMaxStress() { return maxStress; }
+    public int getCost(){
+        return cost;
+    }
+
+    public int getValue(){
+        return value;
+    }
+
+    public int getMaxStress() {
+        return maxStress;
+    }
 }
