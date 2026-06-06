3385f32de5974e8c1de0df695940c5f10be38c2c
Hoang <hoanganhho260506@gmail.com>
2026-06-06 15:49:06 +0700

    chore(test): remove invalid ShopControllerTest file containing duplicate code

--- sourcecode/test/GameTest.java
+++ sourcecode/test/GameTest.java
@@ -1,8 +1,14 @@
 package test;
 
+import controller.actionmanager.PlayerController;
+import controller.democontroller.DemoController;
+import controller.lifecyclemanager.GameManager;
+import controller.shopmanager.ShopController;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.player_inventory.Player;
+import model.soil.FarmMap;
 import view.assets.Assets;
 import view.screen_controller.HelpController;
 import view.screen_controller.InGameController;
@@ -21,7 +27,15 @@ public class GameTest extends Application {
 
         SceneRouter router = new SceneRouter();
 
-        // Load từng screen và inject dependency
+        // 1. Khởi tạo các Domain Objects (Models & Managers)
+        GameManager gameManager = GameManager.getInstance();
+        Player player = new Player();
+        FarmMap farmMap = new FarmMap();
+        PlayerController playerController = new PlayerController(player, gameManager, farmMap.getGrid());
+        DemoController demoController = new DemoController(gameManager, farmMap);
+        ShopController domainShop = new ShopController(player, gameManager, farmMap.getGrid());
+
+        // 2. Load từng screen và inject dependency
         MainMenuController mainMenu = ScreensLoader
                 .load(router, Screens.MAIN_MENU, Screens.MAIN_MENU_FXML)
                 .getController();
@@ -36,11 +50,15 @@ public class GameTest extends Application {
                 .load(router, Screens.INGAME, Screens.INGAME_FXML)
                 .getController();
         inGame.setRouter(router);
+        // Inject game session vào InGame
+        inGame.setGame(farmMap, player, gameManager, playerController, demoController);
 
         ShopViewController shop = ScreensLoader
                 .load(router, Screens.SHOP, Screens.SHOP_FXML)
                 .getController();
         shop.setRouter(router);
+        // Inject shop session vào ShopView
+        shop.setShop(player, domainShop);
 
         PauseController pause = ScreensLoader
                 .load(router, Screens.PAUSE, Screens.PAUSE_FXML)
@@ -51,7 +69,7 @@ public class GameTest extends Application {
 
         Scene scene = new Scene(router.getRoot());
         stage.setScene(scene);
-        stage.setTitle("Smart Farm");
+        stage.setTitle("Smart Farm (Test Mode)");
         stage.show();
     }
 
