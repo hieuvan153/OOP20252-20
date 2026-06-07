62fd3d6ff03ea386c33d4a6f916ec0f636ef6682
Phung Duc Thinh 202417200 <phungthinh035@gmail.com>
2026-06-07 21:05:17 +0700

    fix: modify some logics in Weather class

--- sourcecode/model/weather/Weather.java
+++ sourcecode/model/weather/Weather.java
@@ -5,12 +5,10 @@ import java.util.Objects;
 
 public class Weather {
     private final WeatherType type;
-    private int remainingDuration;
     private final WeatherEffect effect;
 
-    public Weather(WeatherType type, int duration, WeatherEffect effect) {
+    public Weather(WeatherType type, WeatherEffect effect) {
         this.type = Objects.requireNonNull(type, "weather type must not be null");
-        this.remainingDuration = duration;
         this.effect = Objects.requireNonNull(effect, "weather effect must not be null");
     }
 
@@ -18,29 +16,21 @@ public class Weather {
         return type;
     }
 
-    public boolean isFinished() {
-        return remainingDuration <= 0;
-    }
-
     public void applyWeatherEffect(Cell cell) {
         if (cell == null) return;
         effect.applyEffect(cell);
-    }
-
-    public void tickDuration() {
-        if (remainingDuration > 0) remainingDuration--;
     }
 
     // return a weather object of a WeatherType
     public static Weather of(WeatherType type) {
         switch (type) {
             case RAINY:
-                return new Weather(type, 1, new RainyEffect());
+                return new Weather(type, new RainyEffect());
             case DROUGHT:
-                return new Weather(type, 1, new DroughtEffect());
+                return new Weather(type, new DroughtEffect());
             case SUNNY:
             default:
-                return new Weather(type, 1, new SunnyEffect());
+                return new Weather(type, new SunnyEffect());
         }
     }
 }
