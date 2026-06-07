4c78f0fe1b91a76bb3268d9fb2d9c43bee500e03
hieuvan153 <hieuvan153282278@gmail.com>
2026-06-07 15:30:55 +0700

    bugfix: Fixed Farm Controller

--- sourcecode/controller/farmcontroller/FarmController.java
+++ sourcecode/controller/farmcontroller/FarmController.java
@@ -1,10 +1,11 @@
 package controller.farmcontroller;
 
 import controller.lifecyclemanager.BaseController;
+import controller.lifecyclemanager.DayObserver;
 import controller.lifecyclemanager.GameManager;
 import model.soil.FarmMap;
 
-public class FarmController extends BaseController {
+public class FarmController extends BaseController implements DayObserver {
     private final FarmMap farmMap;
 
     public FarmController(GameManager gameManager, FarmMap farmMap) {
